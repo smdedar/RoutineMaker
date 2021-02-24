08c939d6ae12c6e3848f20804331f205db774bcf
Dedar Alam <thededar@gmail.com>
2021-02-24 19:35:20 +0600

    added

---
+++ Development Files/script.js
@@ -0,0 +1,186 @@
+function datat() {
+    // var sun8c = document.getElementById("sun8c").value;
+    // document.getElementById("sun8cp").innerHTML = sun8c;
+    var t8cg = new Array();   
+    var t8rg = new Array();
+    
+        //Sunday 
+        t8cg[0] = document.getElementById("t8cg[0]").value;
+        t8rg[0] = document.getElementById("t8rg[0]").value;
+        document.getElementById("t8cs[0]").innerHTML = t8cg[0];
+        document.getElementById("t8rs[0]").innerHTML = t8rg[0];
+
+        //Monday
+        t8cg[1] = document.getElementById("t8cg[1]").value;
+        t8rg[1] = document.getElementById("t8rg[1]").value;
+        document.getElementById("t8cs[1]").innerHTML = t8cg[1];
+        document.getElementById("t8rs[1]").innerHTML = t8rg[1];
+
+        //Tuesday
+        t8cg[2] = document.getElementById("t8cg[2]").value;
+        t8rg[2] = document.getElementById("t8rg[2]").value;
+        document.getElementById("t8cs[2]").innerHTML = t8cg[2];
+        document.getElementById("t8rs[2]").innerHTML = t8rg[2];
+    
+        //Wednesday
+        t8cg[3] = document.getElementById("t8cg[3]").value;
+        t8rg[3] = document.getElementById("t8rg[3]").value;
+        document.getElementById("t8cs[3]").innerHTML = t8cg[3];
+        document.getElementById("t8rs[3]").innerHTML = t8rg[3];
+
+        //Thursday
+        t8cg[4] = document.getElementById("t8cg[4]").value;
+        t8rg[4] = document.getElementById("t8rg[4]").value;
+        document.getElementById("t8cs[4]").innerHTML = t8cg[4];
+        document.getElementById("t8rs[4]").innerHTML = t8rg[4];
+
+
+
+        var t9cg = new Array();   
+        var t9rg = new Array();
+    
+        //Sunday 
+        t9cg[0] = document.getElementById("t9cg[0]").value;
+        t9rg[0] = document.getElementById("t9rg[0]").value;
+        document.getElementById("t9cs[0]").innerHTML = t9cg[0];
+        document.getElementById("t9rs[0]").innerHTML = t9rg[0];
+
+        //Monday
+        t9cg[1] = document.getElementById("t9cg[1]").value;
+        t9rg[1] = document.getElementById("t9rg[1]").value;
+        document.getElementById("t9cs[1]").innerHTML = t9cg[1];
+        document.getElementById("t9rs[1]").innerHTML = t9rg[1];
+
+        //Tuesday
+        t9cg[2] = document.getElementById("t9cg[2]").value;
+        t9rg[2] = document.getElementById("t9rg[2]").value;
+        document.getElementById("t9cs[2]").innerHTML = t9cg[2];
+        document.getElementById("t9rs[2]").innerHTML = t9rg[2];
+    
+        //Wednesday
+        t9cg[3] = document.getElementById("t9cg[3]").value;
+        t9rg[3] = document.getElementById("t9rg[3]").value;
+        document.getElementById("t9cs[3]").innerHTML = t9cg[3];
+        document.getElementById("t9rs[3]").innerHTML = t9rg[3];
+
+        //Thursday
+        t9cg[4] = document.getElementById("t9cg[4]").value;
+        t9rg[4] = document.getElementById("t9rg[4]").value;
+        document.getElementById("t9cs[4]").innerHTML = t9cg[4];
+        document.getElementById("t9rs[4]").innerHTML = t9rg[4];
+
+
+
+        var t11cg = new Array();   
+var t11rg = new Array();
+    
+        //Sunday 
+        t11cg[0] = document.getElementById("t11cg[0]").value;
+        t11rg[0] = document.getElementById("t11rg[0]").value;
+        document.getElementById("t11cs[0]").innerHTML = t11cg[0];
+        document.getElementById("t11rs[0]").innerHTML = t11rg[0];
+
+        //Monday
+        t11cg[1] = document.getElementById("t11cg[1]").value;
+        t11rg[1] = document.getElementById("t11rg[1]").value;
+        document.getElementById("t11cs[1]").innerHTML = t11cg[1];
+        document.getElementById("t11rs[1]").innerHTML = t11rg[1];
+
+        //Tuesday
+        t11cg[2] = document.getElementById("t11cg[2]").value;
+        t11rg[2] = document.getElementById("t11rg[2]").value;
+        document.getElementById("t11cs[2]").innerHTML = t11cg[2];
+        document.getElementById("t11rs[2]").innerHTML = t11rg[2];
+    
+        //Wednesday
+        t11cg[3] = document.getElementById("t11cg[3]").value;
+        t11rg[3] = document.getElementById("t11rg[3]").value;
+        document.getElementById("t11cs[3]").innerHTML = t11cg[3];
+        document.getElementById("t11rs[3]").innerHTML = t11rg[3];
+
+        //Thursday
+        t11cg[4] = document.getElementById("t11cg[4]").value;
+        t11rg[4] = document.getElementById("t11rg[4]").value;
+        document.getElementById("t11cs[4]").innerHTML = t11cg[4];
+        document.getElementById("t11rs[4]").innerHTML = t11rg[4];
+
+
+
+
+
+        var t1cg = new Array();   
+var t1rg = new Array();
+    
+        //Sunday 
+        t1cg[0] = document.getElementById("t1cg[0]").value;
+        t1rg[0] = document.getElementById("t1rg[0]").value;
+        document.getElementById("t1cs[0]").innerHTML = t1cg[0];
+        document.getElementById("t1rs[0]").innerHTML = t1rg[0];
+
+        //Monday
+        t1cg[1] = document.getElementById("t1cg[1]").value;
+        t1rg[1] = document.getElementById("t1rg[1]").value;
+        document.getElementById("t1cs[1]").innerHTML = t1cg[1];
+        document.getElementById("t1rs[1]").innerHTML = t1rg[1];
+
+        //Tuesday
+        t1cg[2] = document.getElementById("t1cg[2]").value;
+        t1rg[2] = document.getElementById("t1rg[2]").value;
+        document.getElementById("t1cs[2]").innerHTML = t1cg[2];
+        document.getElementById("t1rs[2]").innerHTML = t1rg[2];
+    
+        //Wednesday
+        t1cg[3] = document.getElementById("t1cg[3]").value;
+        t1rg[3] = document.getElementById("t1rg[3]").value;
+        document.getElementById("t1cs[3]").innerHTML = t1cg[3];
+        document.getElementById("t1rs[3]").innerHTML = t1rg[3];
+
+        //Thursday
+        t1cg[4] = document.getElementById("t1cg[4]").value;
+        t1rg[4] = document.getElementById("t1rg[4]").value;
+        document.getElementById("t1cs[4]").innerHTML = t1cg[4];
+        document.getElementById("t1rs[4]").innerHTML = t1rg[4];
+
+
+
+
+        var t3cg = new Array();   
+var t3rg = new Array();
+    
+        //Sunday 
+        t3cg[0] = document.getElementById("t3cg[0]").value;
+        t3rg[0] = document.getElementById("t3rg[0]").value;
+        document.getElementById("t3cs[0]").innerHTML = t3cg[0];
+        document.getElementById("t3rs[0]").innerHTML = t3rg[0];
+
+        //Monday
+        t3cg[1] = document.getElementById("t3cg[1]").value;
+        t3rg[1] = document.getElementById("t3rg[1]").value;
+        document.getElementById("t3cs[1]").innerHTML = t3cg[1];
+        document.getElementById("t3rs[1]").innerHTML = t3rg[1];
+
+        //Tuesday
+        t3cg[2] = document.getElementById("t3cg[2]").value;
+        t3rg[2] = document.getElementById("t3rg[2]").value;
+        document.getElementById("t3cs[2]").innerHTML = t3cg[2];
+        document.getElementById("t3rs[2]").innerHTML = t3rg[2];
+    
+        //Wednesday
+        t3cg[3] = document.getElementById("t3cg[3]").value;
+        t3rg[3] = document.getElementById("t3rg[3]").value;
+        document.getElementById("t3cs[3]").innerHTML = t3cg[3];
+        document.getElementById("t3rs[3]").innerHTML = t3rg[3];
+
+        //Thursday
+        t3cg[4] = document.getElementById("t3cg[4]").value;
+        t3rg[4] = document.getElementById("t3rg[4]").value;
+        document.getElementById("t3cs[4]").innerHTML = t3cg[4];
+        document.getElementById("t3rs[4]").innerHTML = t3rg[4];
+}
+
+$(function () {
+    $('#color-picker-background').colorpicker().on('changeColor', function (e) {
+        $('#boundary')[0].style.backgroundColor = e.color.toHex();
+    });
+});
+
